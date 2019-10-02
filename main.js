f7e134a364ff3b57eb1a455a8eee62a1228f26eb
Charles Wines <ccrecorder@gmail.com>
2019-10-02 01:13:06 -0400

    styling done to look like the movie

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ const dateOfManufacture = document.querySelector('#dateOfManufacture');
 
 const fetchDroid = function() {
 	fetch('https://swapi.co/api/people/8/').then((response) => response.json()).then((data) => {
-		name.innerText = `${data.name} Unit`;
+		name.innerText = `R5 Astromech Unit`;
 		size.innerText = `Height of ${data.height} cm`;
-		color.innerText = `Color: ${data.color} and white`;
+		color.innerText = `Color: ${data.skin_color}`;
 		shippingWeight.innerText = `Shipping Weight: ${data.mass} kg`;
 		dateOfManufacture.innerText = `Date of Manufacture: ${data.birth_year}`;
 	});
